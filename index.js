d1401790b1df383cd3ded0f04f974dfaefce8a3f
Hasanuj123 <mdhasanujjaman001@gmail.com>
2023-01-23 00:22:14 +0530

    mjmjk

---
+++ index.js
@@ -0,0 +1,17 @@
+const imageContainerEl= document.querySelector(".image-container")
+const btnEl = document.querySelector(".btn");
+btnEl.addEventListener("click", ()=>{
+    let imgNum=0;
+    addNewImages()
+
+});
+function addNewImages() {
+    // for (let index = 0; index<imageNum; index++) {
+        
+   
+   const newImgEl = document.createElement("img");
+   newImgEl.src= `http://picsum.photos/200/300?random=${Math.floor(Math.random() * 2000)}`;
+   imageContainerEl.appendChild(newImgEl);
+        
+    }
+    
